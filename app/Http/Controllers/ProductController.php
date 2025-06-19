ed15ddcdbbc38848690e6f9782c75b4028428e61
zyyyyd <zyrelfrias24@gmail.com>
2025-06-20 00:10:18 +0800

    crud

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,11 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class ProductController extends Controller
 {
     public function index(){
-        return Inertia::render('Products/Index',[]);
+        $products = Product::all();
+        return Inertia::render('Products/Index', compact('products'));
+    }
+    public function create(){
+        return Inertia::render('Products/Create',[]);
+    }
+     public function store(Request $request){
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'price' => 'required|numeric',
+            'description' => 'nullable|string'
+        ]);
+
+        Product::create($request->all());
+        return redirect()->route('products.index')->with('message','Product created successfully');
+    }
+    public function destroy(Product $product){
+        $product->delete();
+        return redirect()->route('products.index')->with('message','Product deleted successfully');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,13 @@ Route::get('/', function () {
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
-    })->name('dashboard');
+        return Inertia::render('dashboard');})->name('dashboard');
     Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+     Route::post('/products', [ProductController::class, 'store'])->name('products.store');
                                                         //name of function
-    Route::get('/products/create', [ProductController::class, 'index'])->name('products.create');
+    Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
+       Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
+    
 });
 
 require __DIR__.'/settings.php';
